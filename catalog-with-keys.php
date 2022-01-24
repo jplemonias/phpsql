bb3a7b8bb3e37b3cf77b58528649a277c77b63c2
jplemonias <jpaul.lemonias.dev@google.com>
2022-01-24 22:28:41 +0100

    add index.html / index.php modif /replace weight => summary

--- catalog-with-keys.php
+++ catalog-with-keys.php
@@ -2,22 +2,22 @@
     $necronomicon = [
         "name" => "Necronomicon",
         "price" => 6660,
-        "weight" => 1870,
         "discount" => null,
+        "summary" => "The Necronomicon, a completely fictional book mentioned in the stories of pulp horror author H.P. Lovecraft.",
         "picture_url" => "https://universoabierto.com/wp-content/uploads/2021/02/necronomicon.jpg",
     ];
     $cthulhu = [
         "name" => "The Call of Cthulhu",
-        "price" => 9990,
-        "weight" => 374,
+        "price" => 999,
         "discount" => 10,
+        "summary" => "While sorting the affairs of his late Uncle, a man accidentally stumbles across a series of dark secrets connected to an ancient horror waiting to be freed. A faithful rendition of H.P. Lovecraft's short story.",
         "picture_url" => "https://images-na.ssl-images-amazon.com/images/I/81rHR4HYYSL.jpg",
     ];
     $doorstep = [
         "name" => "The Thing on the Doorstep",
-        "price" => 6660,
-        "weight" => 187,
+        "price" => 666,
         "discount" => null,
+        "summary" => "The Thing on the Doorstep is a horror short story by American writer H. P. Lovecraft, part of the Cthulhu Mythos universe.",
         "picture_url" => "https://m.media-amazon.com/images/M/MV5BMjA4ODkxMzc2MV5BMl5BanBnXkFtZTgwODg3NTg2MjE@._V1_.jpg",
     ];
 
--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-    echo "Hello Word!!";
-?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Progression PHP de base</title>
+ </head>
+ <body>
+    <?php
+        include "header.php";
+    ?>
+    <?php
+        echo "Hello Word!!";
+    ?>
+    <?php
+        include "footer.php";
+    ?>
+ </body>
+</html>
