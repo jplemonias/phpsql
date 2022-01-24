a8abc640b68d99f43e6365710653a15f41810ddd
jplemonias <jean-paul.lemonias@le-campus-numerique.fr>
2022-01-24 16:55:01 +0100

    add => 3.2 — Catalogue avec tableaux associatifs BETTER

--- catalog-with-keys.php
+++ catalog-with-keys.php
@@ -3,27 +3,64 @@
         "name" => "Necronomicon",
         "price" => 66.6,
         "weight" => 1870,
-        "discount" => 0,
+        "discount" => null,
         "picture_url" => "https://universoabierto.com/wp-content/uploads/2021/02/necronomicon.jpg",
     ];
     $cthulhu = [
         "name" => "The Call of Cthulhu",
         "price" => 9.99,
         "weight" => 374,
-        "discount" => 0,
+        "discount" => 10,
         "picture_url" => "https://images-na.ssl-images-amazon.com/images/I/81rHR4HYYSL.jpg",
     ];
     $doorstep = [
         "name" => "The Thing on the Doorstep",
         "price" => 6.66,
         "weight" => 187,
-        "discount" => 0,
+        "discount" => null,
         "picture_url" => "https://m.media-amazon.com/images/M/MV5BMjA4ODkxMzc2MV5BMl5BanBnXkFtZTgwODg3NTg2MjE@._V1_.jpg",
     ];
 
-    print_r($necronomicon);
-    echo "<br><br>";
-    print_r($cthulhu);
-    echo "<br><br>";
-    print_r($doorstep);
+    foreach ($necronomicon as $key => $value){
+        if ( $key === "picture_url" ) {
+            echo "<img src=\"$value\" alt=\"Necronomicon\" width=\"222\">";
+        }
+        else if ( $key === "discount") {
+                if  ($value != null ) {
+                echo "<p>$key : $value</p>";
+            }
+        }
+        else {
+            echo "<p>$key : $value</p>";
+        }
+    }
+
+    foreach ($cthulhu as $key => $value){
+        if ( $key === "picture_url" ) {
+            echo "<img src=\"$value\" alt=\"Necronomicon\" width=\"222\">";
+        }
+        else if ( $key === "discount") {
+                if  ($value != null ) {
+                echo "<p>$key : $value</p>";
+            }
+        }
+        else {
+            echo "<p>$key : $value</p>";
+        }
+    }
+
+    foreach ($doorstep as $key => $value){
+        if ( $key === "picture_url" ) {
+            echo "<img src=\"$value\" alt=\"Necronomicon\" width=\"222\">";
+        }
+        else if ( $key === "discount") {
+                if  ($value != null ) {
+                echo "<p>$key : $value</p>";
+            }
+        }
+        else {
+            echo "<p>$key : $value</p>";
+        }
+    }
+
 ?>
