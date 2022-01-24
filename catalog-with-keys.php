62b25598cd78bdbc1692310e11afd426f7acdd8f
jplemonias <jean-paul.lemonias@le-campus-numerique.fr>
2022-01-24 16:59:00 +0100

    modif centimes

--- catalog-with-keys.php
+++ catalog-with-keys.php
@@ -1,21 +1,21 @@
 <?php
     $necronomicon = [
         "name" => "Necronomicon",
-        "price" => 66.6,
+        "price" => 6660,
         "weight" => 1870,
         "discount" => null,
         "picture_url" => "https://universoabierto.com/wp-content/uploads/2021/02/necronomicon.jpg",
     ];
     $cthulhu = [
         "name" => "The Call of Cthulhu",
-        "price" => 9.99,
+        "price" => 9990,
         "weight" => 374,
         "discount" => 10,
         "picture_url" => "https://images-na.ssl-images-amazon.com/images/I/81rHR4HYYSL.jpg",
     ];
     $doorstep = [
         "name" => "The Thing on the Doorstep",
-        "price" => 6.66,
+        "price" => 6660,
         "weight" => 187,
         "discount" => null,
         "picture_url" => "https://m.media-amazon.com/images/M/MV5BMjA4ODkxMzc2MV5BMl5BanBnXkFtZTgwODg3NTg2MjE@._V1_.jpg",
