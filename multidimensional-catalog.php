08820da9b436729fe21bca6be6d6aa45f1d3f8ff
jplemonias <jpaul.lemonias.dev@google.com>
2022-01-24 18:43:36 +0100

    add = > multidimensional-catalog.php

---
+++ multidimensional-catalog.php
@@ -0,0 +1,45 @@
+<?php
+    $books = [
+        $necronomicon = [
+            "name" => "Necronomicon",
+            "price" => 6660,
+            "weight" => 1870,
+            "discount" => null,
+            "picture_url" => "https://universoabierto.com/wp-content/uploads/2021/02/necronomicon.jpg",
+        ],
+        $cthulhu = [
+            "name" => "The Call of Cthulhu",
+            "price" => 9990,
+            "weight" => 374,
+            "discount" => 10,
+            "picture_url" => "https://images-na.ssl-images-amazon.com/images/I/81rHR4HYYSL.jpg",
+        ],
+        $doorstep = [
+            "name" => "The Thing on the Doorstep",
+            "price" => 6660,
+            "weight" => 187,
+            "discount" => null,
+            "picture_url" => "https://m.media-amazon.com/images/M/MV5BMjA4ODkxMzc2MV5BMl5BanBnXkFtZTgwODg3NTg2MjE@._V1_.jpg",
+        ]
+    ];
+    foreach ($books as $key => $value){
+        foreach ($value as $k => $v){
+            if ( $k === "picture_url" ) {
+                echo "<img src=\"$v\" alt=\"Necronomicon\" width=\"222\">";
+                // echo "\n";
+            }
+            else if ( $k === "discount") {
+                    if  ($v != null ) {
+                    echo "<p>$k : $v</p>";
+                    //echo "\n";
+                }
+            }
+            else {
+                echo "<p>$k : $v</p>";
+                // echo "\n";
+            }
+        }
+        // echo "\n";
+    }
+
+?>
