41289bdd5ae3fc05823820eae846683691eedb5d
jplemonias <jean-paul.lemonias@le-campus-numerique.fr>
2022-01-25 14:52:48 +0100

    edit index.php => link require and include for books.php

--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -3,39 +3,88 @@
         $necronomicon = [
             "name" => "Necronomicon",
             "price" => 6660,
-            "weight" => 1870,
             "discount" => null,
+            "summary" => "The Necronomicon, a completely fictional book mentioned in the stories of pulp horror author H.P. Lovecraft.",
             "picture_url" => "https://universoabierto.com/wp-content/uploads/2021/02/necronomicon.jpg",
         ],
         $cthulhu = [
             "name" => "The Call of Cthulhu",
-            "price" => 9990,
-            "weight" => 374,
+            "price" => 999,
             "discount" => 10,
+            "summary" => "While sorting the affairs of his late Uncle, a man accidentally stumbles across a series of dark secrets connected to an ancient horror waiting to be freed. A faithful rendition of H.P. Lovecraft's short story.",
             "picture_url" => "https://images-na.ssl-images-amazon.com/images/I/81rHR4HYYSL.jpg",
         ],
         $doorstep = [
             "name" => "The Thing on the Doorstep",
-            "price" => 6660,
-            "weight" => 187,
+            "price" => 666,
             "discount" => null,
+            "summary" => "The Thing on the Doorstep is a horror short story by American writer H. P. Lovecraft, part of the Cthulhu Mythos universe.",
             "picture_url" => "https://m.media-amazon.com/images/M/MV5BMjA4ODkxMzc2MV5BMl5BanBnXkFtZTgwODg3NTg2MjE@._V1_.jpg",
         ]
     ];
+    
+function popBooks($books){
     foreach ($books as $key => $value){
-        foreach ($value as $k => $v){
+            echo "<div class=\"col\">";
+                echo "<div class=\"card h-100\">";
+                    echo printImg($value);
+                    echo "<div class=\"card-body\">";
+                        echo printTitle($value);
+                        echo printSummary($value);
+                        echo printDiscount($value);
+                    echo "</div>";
+                    echo "<div class=\"card-footer\">";
+                        echo printPrice($value);
+                    echo "</div>";
+                echo "</div>";
+            echo "</div>";
+        
+    }
+    // var_dump($books);
+}
+
+function printImg ($data) {
+    foreach ($data as $k => $v){
             if ( $k === "picture_url" ) {
-                echo "<img src=\"$v\" alt=\"Necronomicon\" width=\"222\">";
+                return "<img width=\"100%\" src=\"$v\" class=\"card-img-top\" alt=\"Cover : $v\">";
             }
-            else if ( $k === "discount") {
-                    if  ($v != null ) {
-                    echo "<p>$k : $v</p>";
-                }
+        }
+}
+
+function printTitle ($data) {
+    foreach ($data as $k => $v){
+            if ( $k === "name" ) {
+                return "<h5 class=\"card-title\">$v</h5>";
             }
-            else {
-                echo "<p>$k : $v</p>";
+        }
+}
+
+function printSummary ($data) {
+    foreach ($data as $k => $v){
+            if ( $k === "summary" ) {
+                return "<p class=\"card-text\">$v.</p>";
+            }
+        }
+}
+
+function printPrice ($data) {
+    foreach ($data as $k => $v){
+            if ( $k === "price" ) {
+
+                // Créer V
+
+                return "<small class=\"text-muted\">$v</small>";
+            }
+        }
+}
+
+function printDiscount ($data) {
+    foreach ($data as $k => $v){
+        if ( $k === "discount" ) {
+            if ( $v != null) {
+                return "<small class=\"badge rounded-pill bg-success\">discount : $v%</small>";
             }
         }
     }
-    var_dump($books);
+}
 ?>
--- simple-catalog.php
+++ simple-catalog.php
@@ -1,12 +1,16 @@
 <?php
-    echo"Tableau original :<br>";
     $products = ["Necronomicon", "The Thing on the Doorstep", "The Call of Cthulhu"];
+
+    echo"Tableau original :<br>";
     print_r($products);
+
     echo"<br><br>Par ordre alphabétique croissant :<br>";
     sort($products);
     print_r($products);
+
     echo"<br><br>Premier élément :<br>";
     print_r($products[0]);
+    
     echo"<br><br>Dernier élément :<br>";
     print_r($products[count($products)-1]);
 ?>
