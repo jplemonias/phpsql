104880ba122f493f5f5d6c0002829ded10fadc3f
jplemonias <jean-paul.lemonias@le-campus-numerique.fr>
2022-02-13 16:25:04 +0100

    e

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hom - I LOVEcraft</title>
+    <title>Home - I LOVEcraft</title>
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <?php
     include "inc/bootstrapLinks.php";
