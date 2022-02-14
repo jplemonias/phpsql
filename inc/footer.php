eb472da97b150cb77d52d1c2455bd32f5dcbe04b
jplemonias <jean-paul.lemonias@le-campus-numerique.fr>
2022-02-14 08:18:07 +0100

    correction lolo campus num

--- inc/footer.php
+++ inc/footer.php
@@ -2,12 +2,12 @@
     <div class="container">
         <div class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
             <p class="col-md-4 mb-0 text-muted">2022 JP</p>
-
-            <a href="https://le-campus-numerique.fr/"
-                class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                <img height="35px" src="public/img/camNumInTheAlps-jp.png" style="padding-left: 13px;"/>
-            </a>
-
+            <div>
+                <a href="https://le-campus-numerique.fr/"
+                    class="col-md-4 d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                    <img height="35px" src="public/img/camNumInTheAlps-jp.png"/>
+                </a>
+            </div>
             <ul class="nav col-md-4 justify-content-end">
                 <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
                 <li class="nav-item"><a href="404" class="nav-link px-2 text-muted">Features</a></li>
